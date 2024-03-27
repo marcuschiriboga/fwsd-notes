0b8a2d5f3f534b78698e1c880de1bbf6daa2c54a
marcuschiriboga <marcus.chiriboga@gmail.com>
2024-03-27 13:07:55 -0400

    week_10 changes

--- fwsd/week_8/server/models/models.js
+++ fwsd/week_8/server/models/models.js
@@ -7,7 +7,12 @@ const CarSchema = new Schema({
   model: {
     type: String,
     required: true,
-    minLength: 2
+    minLength: 2,
+    asefklhasldkf,
+    asdflnsdaf,
+    asdlkfnasdl,
+    asdflkjads,
+
   },
   isConvertible: {
     type: Boolean,
@@ -20,4 +25,19 @@ const CarSchema = new Schema({
   }
 })
 const Car = model("Car", CarSchema);
-export default Car;
+// const Owner = model("Owner", OwnerSchema)
+const addPlateField = (schema) => {
+  return schema.clone({ plate: {type: String, required: false}})
+  };
+  const addMakerField = (schema) => {
+    return schema.clone({ maker: {type: Mongoose.Schema.Types.ObjectId, ref: Maker}})
+    };
+  
+  const newSchema = addPlateField(CarSchema)
+
+export default {Car, newSchema};
+
+"adding a new field" "schema reducer" "adding a owner or licence plate field to the model"
+"create a new relationship" "car-owner, car-driver, car-passenger, car-mechanic, car-manufacturer" "" 
+Car.findById("pretendID4").populate('owner').populate("maker")  "Car._id" "mongodb automatically adds a ._id"
+"nest documents" 
